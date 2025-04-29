0770ad2c7f4b53e4e58a0aa6723a0c91255178c0
DanielPietrzyk <danielpietrzyk07@gmail.com>
2025-04-29 17:15:02 +0200

    wip: prepared remove_user

--- utils/controller.py
+++ utils/controller.py
@@ -7,3 +7,11 @@ def add_user(users_data: list) -> None:
     new_location = input('Podaj lokalizację nowego użytkownika: ')
     new_posts = int(input('Podaj liczbę postów nowego użytkownika: '))
     users_data.append({'name': new_name , 'location': new_location, 'posts': new_posts})
+
+def remove_user(users_data: list) -> None:
+    użytkownik_do_usunięcia = input('Podaj użytkownika do usunięcia: ')
+
+    for user in users_data:
+
+        if user['name'] == użytkownik_do_usunięcia:
+            users_data.remove(user)
