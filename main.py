1fa956062f3184cf5ec54f53b8b834e20022ba38
DanielPietrzyk <danielpietrzyk07@gmail.com>
2025-04-29 16:44:46 +0200

    wip: prepared add_user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 from utils.model import users
 
 
@@ -17,6 +17,7 @@ def main():
             break
         if choice == "1":
             get_user_info(users)
+        if choice == "2": add_user(users)
 
 
 if __name__ == "__main__":
